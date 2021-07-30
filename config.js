c79538afb69c9cce1ede049eadb4e09c2b27d72a
Irupc <68991705+Irupc@users.noreply.github.com>
2021-07-31 01:19:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,5 +11,7 @@ module.exports = {
     enable_delete_alert: process.env.ENABLE_DELETE_ALERT || "true",
     ocr_space_api_key: process.env.OCR_SPACE_API_KEY || "7e4bfe810488957",
     infospace_api_key: process.env.INFOSPACE_API_KEY || "6zzl92aoe16bz0cgu3soyf73zu5zeu7x5p9wqn0wdq8k6v4fxuf3o",
-    movie_db_url: process.env.MOVIES_IRUPC || "mongodb+srv://Gamy_Gamin:Gamy_Gamin@cluster0.ygf7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    movie_db_url: process.env.MOVIES_IRUPC || "mongodb+srv://Gamy_Gamin:Gamy_Gamin@cluster0.ygf7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    admin_bottom: process.env.MOVIES_BOTTOM || "©️ _Whats App Movie Bot_ by *iruPC.net*",
+    admin_top: process.env.MOVIES_TOP || "💓 Visit *WhatsApp Movie Bot* for More Links 👇\n*wa.me/+94712882557*"
 }
